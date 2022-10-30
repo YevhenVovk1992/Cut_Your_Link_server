58e5235499f2a519b5d1262dca95d4686e3689a5
YevhenVovk <vovk.valki@ukr.net>
2022-10-30 18:14:56 +0200

    Add Dockerfile and docker-compose

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import asyncio
 from bson import ObjectId
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, executor, types
+from emoji import emojize
 
 from db import setup_db
 
@@ -19,14 +20,30 @@ logging.basicConfig(level=logging.INFO)
 async def start_command(message: types.Message):
     user = message.from_user.get_mention(as_html=True)
     await message.answer(
-        f"Hello {user}.\nWrite here the address which you need to do shortened",
+        emojize(
+            f"Hello {user}. :grinning_face:\nWe are happy to receive a message from you!"
+        ),
+        parse_mode=types.ParseMode.HTML)
+    await message.answer(
+        emojize(
+            f"Write here the address which you need to do shortened :backhand_index_pointing_down:"
+        ),
         parse_mode=types.ParseMode.HTML)
 
 
 async def help_command(message: types.Message):
     user = message.from_user.get_mention(as_html=True)
+    help_text = emojize(
+        "Write the link :fountain_pen: in the format:\n"
+        "http://host/other_path  or  https://host/3/example/\n"
+        "and you will get short URL.\n"
+        "The link you are received contains the ID. This ID help you to get origin URL.\n"
+        "Example: http://OurHost/ID :right_arrow_curving_down:\n input ID text:right_arrow:get link\n"
+        ":beaming_face_with_smiling_eyes:",
+        language='en'
+    )
     await message.answer(
-        f"Dear, {user}.\nThis service will help you shorten the link to the resource",
+        f"Dear, {user}.\nThis service will help you shorten the link to the resource.\n{help_text}",
         parse_mode=types.ParseMode.HTML)
 
 
@@ -48,7 +65,7 @@ async def get_user_url(message: types.Message):
 
 
 async def send_user_url(message: types.Message):
-    get_user_link_id = message.text.replace(os.environ.get('SHORTENER_SERVER'), '')
+    get_user_link_id = message.text
     try:
         db = await setup_db()
         collection = db['shortener']
